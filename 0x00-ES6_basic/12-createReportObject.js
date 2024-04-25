6086af5048f78b9eef3f5abb528b274e1eb74b2d
Havenhai <havenhailemariam12@gmail.com>
2024-04-25 11:57:46 -0700

    javascript

--- 0x00-ES6_basic/12-createReportObject.js
+++
@@ -1,9 +0,0 @@
-export default function createReportObject(employeesList) {
-  const obj = {
-    allEmployees: employeesList,
-    getNumberOfDepartments() {
-      return Object.keys(employeesList).length;
-    },
-  };
-  return obj;
-}
